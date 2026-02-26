8a16767a245f7882134be5431fa79419fb622b90
HypeVaHo <124986792+HypeVaHo@users.noreply.github.com>
2026-02-26 12:21:31 +0300

    Add files via upload

---
+++ fix-encoding.ps1
@@ -0,0 +1,13 @@
+$files = @(
+  "index.html",
+  "discipline.html",
+  "disputes.html",
+  "liability.html",
+  "styles.css",
+  "script.js"
+)
+
+foreach ($f in $files) {
+  $raw = Get-Content -Raw -Encoding Default $f
+  Set-Content -Encoding utf8 $f $raw
+}
